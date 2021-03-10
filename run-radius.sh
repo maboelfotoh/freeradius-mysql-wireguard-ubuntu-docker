c9b17c5110492206fa092ced596c46d5612e6797
M. Aboelfotoh <>
2021-03-10 12:09:33 +0200

    Fixes. Move to Ubuntu 20.10

--- run-radius.sh
+++ run-radius.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 RADIUS_CONF_DIR=
 WIREGUARD_CONF_DIR=
+MYSQL_CERTS_DIR=
 WIREGUARD_PORT=
 DOCKER_IMAGE=
 docker run --cap-add net_admin --cap-add sys_module \
-  -v $RADIUS_CONF_DIR:/etc/freeradius/3.0 -v $WIREGUARD_CONF_DIR:/etc/wireguard \
+  -v $RADIUS_CONF_DIR:/etc/freeradius/3.0 \
+  -v $MYSQL_CERTS_DIR:/etc/ssl/certs/mysql-certs \
+  -v $WIREGUARD_CONF_DIR:/etc/wireguard \
   -p $WIREGUARD_PORT:$WIREGUARD_PORT/udp \
   $DOCKER_IMAGE
